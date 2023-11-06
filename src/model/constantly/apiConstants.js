d2a71491d232fb43a1f6c543161f2b0f52bbc9ff
mrkdddddddd <tedmic999@naver.com>
2023-11-07 00:45:43 +0900

    constants정리

--- src/model/constantly/apiConstants.js
+++ src/model/constantly/apiConstants.js
@@ -2,3 +2,32 @@ export const api_key="RGAPI-6e1b716a-027f-4306-930b-458ee9fb0229"; //라이엇 A
 
 export const solo_rank_url="https://kr.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/CHALLENGER/I"; //랭킹, 랭킹 상세페이지에서 사용
 export const summoner_url="https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name"; //유저 네임으로 간단 정보 추출 가능
+
+export const apiKey = "RGAPI-6e1b716a-027f-4306-930b-458ee9fb0229";
+// NicknameUrl + encodedName[i] + "?api_key=" + apiKey (id, acountid puuid name profileIconId level)
+export const NicknameUrl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
+// IdUrl + id + "?api_key=" + apiKey (티어,개인랭 승패 name) 
+export const IdUrl = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/";
+// MatchesUrl + puuid + "/ids?count=5&api_key=" + apiKey; count가 match  갯수 (matchid)
+export const MatchesUrl = "https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/";
+// MatchDataUrl + matchid + "?api_key=" + apiKey (매치데이터)
+export const MatchDataUrl = "https://asia.api.riotgames.com/lol/match/v5/matches/"
+
+// 이미지
+export const version = "https://ddragon.leagueoflegends.com/cdn/13.21.1/data/ko_KR/";
+export const championUrl = version + "champion.json";
+export const iconUrl = version + "profileicon.json";
+
+//티어api값이랑 png맵핑
+export const tierImgMapping = new Map([
+    ["NONE", "/images/tier-icons/provisional.png"],
+    ["IRON", "/images/tier-icons/emblem-iron.png"],
+    ["BRONZE", "/images/tier-icons/emblem-bronze.png"],
+    ["SILVER", "/images/tier-icons/emblem-silver.png"],
+    ["GOLD", "/images/tier-icons/emblem-gold.png"],
+    ["PLATINUM", "/images/tier-icons/emblem-platinum.png"],
+    ["DIAMOND", "/images/tier-icons/emblem-diamond.png"],
+    ["MASTER", "/images/tier-icons/emblem-master.png"],
+    ["GRANDMASTER", "/images/tier-icons/emblem-grandmaster.png"],
+    ["CHALLENGER", "/images/tier-icons/emblem-challenger.png"],
+]);
