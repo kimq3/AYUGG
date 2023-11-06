b6d0c48a07ee8ac3e3e19adb19a67afda2d22df3
김규섭 <rlarbtjq6597@gmail.com>
2023-11-06 22:09:19 +0900

    디렉터리 구조변경 및 절대경로 시스템 구축 및 상대경로 입력 페이지 수정

--- src/view/page/main/mainStyle/mainStyle.js
+++ src/view/page/main/mainStyle/mainStyle.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import logo from '../../../image/logo/body-logo.png';
+import logo from 'assets/images/logo/body-logo.png';
 
 export const Logo = styled.img`
     width: 600px;
