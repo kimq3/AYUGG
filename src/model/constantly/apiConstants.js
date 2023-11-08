d5a9912b1a0b44458baf743b7bbb60f1704ad34a
mrkdddddddd <tedmic999@naver.com>
2023-11-08 20:42:46 +0900

    12121

--- src/model/constantly/apiConstants.js
+++ src/model/constantly/apiConstants.js
@@ -42,10 +42,6 @@ export function ChampionApi() {
     return champData;
 }
 
-
-export const solo_rank_url="https://kr.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/CHALLENGER/I"; //랭킹, 랭킹 상세페이지에서 사용
-export const summoner_url="https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name"; //유저 네임으로 간단 정보 추출 가능
-
 export const apiKey = "RGAPI-6e1b716a-027f-4306-930b-458ee9fb0229";
 // NicknameUrl + encodedName[i] + "?api_key=" + apiKey (id, acountid puuid name profileIconId level)
 export const NicknameUrl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/";
