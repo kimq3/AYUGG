3ee9393c65b8d89c78621281da30f387e97f25cd
mrkdddddddd <tedmic999@naver.com>
2023-11-17 17:53:42 +0900

    searchPage 데이터 받고 topBox 완료

--- src/view/page/search/searchPage.js
+++ src/view/page/search/searchPage.js
@@ -1,14 +1,25 @@
+import { useState, useEffect } from "react";
 import Nav from "view/nav";
 import TopBox from "view/page/search/topBox"
 import { ContainerDiv } from "view/page/search/searchStyle/topBoxStyle"
 import Button from "view/page/search/refreshButton"
+import GetSearchData from "view/page/search/getSearchData";
 
 function SearchPage(){
+  const [searchData, setSearchData] = useState([]);
+  useEffect(() => {
+    GetSearchData("hideonbush")
+    .then((data)=>{
+      setSearchData(data);
+      console.log(data);
+    })
+  }, []);
+
   return (
     <div>
       <Nav></Nav>
       <ContainerDiv>
-        <TopBox />
+        <TopBox data={searchData}/>
         <Button />
         <br />
 
--- src/view/page/search/topBox.js
+++ src/view/page/search/topBox.js
@@ -1,41 +1,44 @@
 import { UserDiv, FirstDiv, PlayerIconImg, NicknameSpan, SecondDiv, TierDiv, NowTierDiv, TierNameDiv, TierImg, TierListUl, NowTierLi, LabelDiv, OptionItemLi, MoreListUl, GraphBox, MoreImg } from "view/page/search/searchStyle/topBoxStyle.js";
+import { tierImgMapping } from 'model/constantly/apiConstants';
+import { useState } from "react";
 
 //버튼전까지의 UI구성
-function TopBox() {
+function TopBox(props) {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const clickDropdown = () => {
+    setDropdownVisible(!isDropdownVisible);
+  };
+  
   return (
     <div>
       <UserDiv>
         <FirstDiv>
-          <NicknameSpan>Hide on bush</NicknameSpan>
-          <PlayerIconImg src={`${process.env.PUBLIC_URL}` + 'assets/images/example.webp'} />
+          <NicknameSpan>{props.data.nickname}</NicknameSpan>
+          <PlayerIconImg src={GetPlayerIconImg(props.data.profileIconId)} />
         </FirstDiv>
         <SecondDiv>
           <TierDiv>
             <NowTierDiv>
-              <TierImg src={`${process.env.PUBLIC_URL}` + 'assets/images/example.webp'} />
-              {/* <TierTextDiv>
-                <TierNameDiv />
-                <TierPointDiv />
-                <WinRateDiv />
-              </TierTextDiv> */}
+              <TierImg src={`${process.env.PUBLIC_URL}` + tierImgMapping.get(props.data.tier)} />
               <div>
-                <TierNameDiv>TierNameDiv</TierNameDiv>
-                <div>TierPointDiv</div>
-                <div>WinRateDiv</div>
+                <TierNameDiv>{props.data.tier} {props.data.rank}</TierNameDiv>
+                <div>{props.data.leaguePoints}</div>
+                <div>승률 {Math.round(props.data.wins / (props.data.wins + props.data.losses) * 100)}% {'('}{props.data.wins}승 {props.data.losses}{'패)'}</div>
               </div>
             </NowTierDiv>
             <TierListUl>
               <NowTierLi>
-                <b>S22 </b>
-                <span>Gold 1</span>
+                <b>S22  </b>
+                <span>{props.data.tier} {props.data.rank}</span>
               </NowTierLi>
               {/* <MoreTierLi> */}<li>
                 {/* <SelectionDiv> */}<div>
-                  <LabelDiv>
+                  <LabelDiv onClick={clickDropdown}>
                     <b>More</b>
                     <MoreImg src={`${process.env.PUBLIC_URL}` + 'assets/images/arrow-down-icon-original.svg'} />
                   </LabelDiv>
-                  <MoreListUl>
+                  <MoreListUl isVisible={isDropdownVisible}>
                     <OptionItemLi>
                       <b>S9</b>
                       <span>Gold 1</span>
@@ -57,6 +60,13 @@ function TopBox() {
       </UserDiv>
     </div>
   );
+}
+
+
+function GetPlayerIconImg(profileIconId) {
+  const version = "https://ddragon.leagueoflegends.com/cdn/13.22.1/img/profileicon/"
+  const champUrl = version + profileIconId + ".png";
+  return champUrl;
 }
 
 export default TopBox;
